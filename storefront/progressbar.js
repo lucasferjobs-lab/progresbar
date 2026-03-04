d3c73a2165e9b86fc50d6fad724654257c2035a1
Lucas Fernandez <lucas@selfie.cl>
2026-03-04 14:49:56 -0300

    FEAT desing fix10

--- storefront/progressbar.js
+++ storefront/progressbar.js
@@ -9,7 +9,7 @@
     api.init(root);
   }
 })(typeof window !== 'undefined' ? window : globalThis, function () {
-  const APP_VERSION = '2026-03-04-05';
+  const APP_VERSION = '2026-03-04-06';
 
   function clampPct(pct) {
     const n = Number(pct || 0);
